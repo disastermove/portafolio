c238f2c36655d571141a76b9d6e85d248d8f78dd
disastermove <99416935+disastermove@users.noreply.github.com>
2023-01-28 01:59:10 +0100

    Draft discontinued project

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -40,46 +40,7 @@ const Box = () => {
                     </div>
                 </div>
 
-                <div className="projects_cards">
-                    <a
-                        href="https://rickandmortyapis.netlify.app/"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        <img
-                            src={"/imagenes/rickandmorty.webp"}
-                            className="projects_cards-image"
-                            alt="project"
-                        ></img>
-                    </a>
-                    <div className="projects_cards-info">
-                        <h3 className="projects_name">Rick and Morty API</h3>
-                        <p className="projects_project-info">
-                            Esto es un proyecto hecho con la api de rick and morty con react
-                            <br></br> <br></br>
-                            <br></br>
-                        </p>
-                        <div className="projects_project-stack">
-                            <p>ReactJS, JS, CSS</p>
-                            <a
-                                className="projects_project-stack-github"
-                                href="https://github.com/disastermove/rickandmortyapi"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <FontAwesomeIcon icon={faGithub} />
-                            </a>
-                            <a
-                                className="projects_project-stack-deploy"
-                                href="https://rickandmortyapis.netlify.app/"
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <FontAwesomeIcon icon={faGoogle} />
-                            </a>
-                        </div>
-                    </div>
-                </div>
+
                 <div className="projects_cards">
                     <a
                         href="https://rickandmortyapiv2.netlify.app/"
@@ -93,7 +54,7 @@ const Box = () => {
                         ></img>
                     </a>
                     <div className="projects_cards-info">
-                        <h3 className="projects_name">Rick and Morty  API v2</h3>
+                        <h3 className="projects_name">Rick and Morty API v2</h3>
                         <p className="projects_project-info">
                             Esto es un proyecto rehecho con la api de rick y morty con responsive y muchas mas cosas
                             <br></br> <br></br>
