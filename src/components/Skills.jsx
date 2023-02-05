269169d13edbddcfb71482ec11a05702fb558038
disastermove <aspacia2022@gmail.com>
2023-02-06 00:30:15 +0100

    x2

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,49 +6,49 @@ const Skills = () => {
             <h2 className='text-center'>My Skills</h2>
             <Container className='grids'>
                 <Row className='rower'>
-                    <Col xs={2} >
+                    <Col xs={2}>
                         <div className="skill-box">
                             <Image src="/iconos/html.webp" alt="HTML" fluid />
                             <p>HTML</p>
                         </div>
                     </Col>
-                    <Col xs={2} >
+                    <Col xs={2}>
                         <div className="skill-box">
                             <Image src="/iconos/css.webp" alt="CSS" fluid />
                             <p>CSS</p>
                         </div>
                     </Col>
-                    <Col xs={2} >
+                    <Col xs={2}>
                         <div className="skill-box">
                             <Image src="/iconos/js.webp" alt="JavaScript" fluid />
                             <p>JavaScript</p>
                         </div>
                     </Col>
-                    <Col xs={2} >
+                    <Col xs={2}>
                         <div className="skill-box">
                             <Image src="/iconos/react.webp" alt="React" fluid />
                             <p>React</p>
                         </div>
                     </Col>
-                    <Col xs={2} >
+                    <Col xs={2}>
                         <div className="skill-box">
                             <Image src="/iconos/reactnative.webp" alt="React Native" fluid />
                             <p>React Native</p>
                         </div>
                     </Col>
-                    <Col xs={2} >
+                    <Col xs={2}>
                         <div className="skill-box">
                             <Image src="/iconos/figma.webp" alt="Figma" fluid />
                             <p>Figma</p>
                         </div>
                     </Col>
-                    <Col xs={2} >
+                    <Col xs={2}>
                         <div className="skill-box">
                             <Image src="/iconos/node.webp" alt="Node.js" fluid />
                             <p>Node.js</p>
                         </div>
                     </Col>
-                    <Col xs={2} >
+                    <Col xs={2}>
                         <div className="skill-box">
                             <Image src="/iconos/sass.webp" alt="Sass" fluid />
                             <p>Sass</p>
