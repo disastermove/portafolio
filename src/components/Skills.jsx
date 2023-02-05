58ef377864527d727011fdebb9af088208f4b9ea
disastermove <aspacia2022@gmail.com>
2023-02-06 00:25:18 +0100

    Change margin and add decoration

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,49 +9,49 @@ const Skills = () => {
                     <Col xs={2} >
                         <div className="skill-box">
                             <Image src="/iconos/html.webp" alt="HTML" fluid />
-                            <p style={{ marginTop: 10 }}>HTML</p>
+                            <p>HTML</p>
                         </div>
                     </Col>
                     <Col xs={2} >
                         <div className="skill-box">
                             <Image src="/iconos/css.webp" alt="CSS" fluid />
-                            <p style={{ marginTop: 10 }}>CSS</p>
+                            <p>CSS</p>
                         </div>
                     </Col>
                     <Col xs={2} >
                         <div className="skill-box">
                             <Image src="/iconos/js.webp" alt="JavaScript" fluid />
-                            <p style={{ marginTop: 10 }}>JavaScript</p>
+                            <p>JavaScript</p>
                         </div>
                     </Col>
                     <Col xs={2} >
                         <div className="skill-box">
                             <Image src="/iconos/react.webp" alt="React" fluid />
-                            <p style={{ marginTop: 10 }}>React</p>
+                            <p>React</p>
                         </div>
                     </Col>
                     <Col xs={2} >
                         <div className="skill-box">
                             <Image src="/iconos/reactnative.webp" alt="React Native" fluid />
-                            <p style={{ marginTop: 10 }}>React Native</p>
+                            <p>React Native</p>
                         </div>
                     </Col>
                     <Col xs={2} >
                         <div className="skill-box">
                             <Image src="/iconos/figma.webp" alt="Figma" fluid />
-                            <p style={{ marginTop: 10 }}>Figma</p>
+                            <p>Figma</p>
                         </div>
                     </Col>
                     <Col xs={2} >
                         <div className="skill-box">
                             <Image src="/iconos/node.webp" alt="Node.js" fluid />
-                            <p style={{ marginTop: 10 }}>Node.js</p>
+                            <p>Node.js</p>
                         </div>
                     </Col>
                     <Col xs={2} >
                         <div className="skill-box">
                             <Image src="/iconos/sass.webp" alt="Sass" fluid />
-                            <p style={{ marginTop: 10 }}>Sass</p>
+                            <p>Sass</p>
                         </div>
                     </Col>
                 </Row>
