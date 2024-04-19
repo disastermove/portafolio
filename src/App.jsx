2ee52d23134bc48cde02833fb8746afedbf65715
disastermove <99416935+disastermove@users.noreply.github.com>
2024-04-19 23:29:33 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import "./App.css";
 import React from "react";
 import Footer from "./components/Footer";
 import Skills from "./components/Skills";
-import { Navbar, Nav, Container, Row, Col, Image } from "react-bootstrap";
+import {
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+  Image,
+  Button,
+} from "react-bootstrap";
 import Box from "./components/Box";
 import { useTranslation } from "react-i18next";
 import LanguageSwitcher from "./components/LanguageSwitcher";
@@ -35,7 +43,7 @@ const App = () => {
       <div className="sec__5__circle5 animate-fade-in-down"></div>
       <div className="sec__5__circle6 animate-fade-in-down"></div>
       <div className="sec__5__circle7 animate-fade-in-down"></div>
-      <LanguageSwitcher />
+      {/* <LanguageSwitcher /> */}
       <Row className="mt-5 align-items-center justify-content-evenly animate-fade-in-down">
         <Col md={4}>
           <div className="description-info">
@@ -51,6 +59,30 @@ const App = () => {
         <Col md={7} id="about">
           <p>{t("info1")}</p>
           <p>{t("info2")}</p>
+          <Button
+            className="cv-btn"
+            download
+            href="/cv_manel.pdf"
+            // onClick={() => window.open("/cv_manel.pdf")}
+          >
+            Descargar CV
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              style={{ marginLeft: "5px" }}
+            >
+              <path
+                d="M5.256 16A5.33 5.33 0 0 1 3 11.65C3 9.2 4.8 6.937 7.5 6.5 8.347 4.486 10.351 3 12.69 3c2.994 0 5.442 2.323 5.61 5.25 1.59.695 2.7 2.4 2.7 4.247a4.5 4.5 0 0 1-2 3.745M12 21V11m0 10-3-3m3 3 3-3"
+                stroke="#fff"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </Button>
         </Col>
       </Row>
       <Box />
